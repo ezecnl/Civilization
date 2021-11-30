1f68831a70c4a58c89436054b0d29a3ad6c3ccee
ezecnl <ecoronel@alumno.huergo.edu.ar>
2021-11-30 17:09:00 -0300

    documentacon y UML agregado

--- azteca.py
+++
@@ -1,9 +0,0 @@
-from civilizacion import Civilizacion
-class Azteca(Civilizacion):
-    def __init__(self):
-        super().__init__()
-        self.ataque = 30
-        self.vida = 100
-        self.velocidad = 30
-        self.comida = 10
-        
